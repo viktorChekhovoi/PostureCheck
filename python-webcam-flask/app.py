e3e145e9cb4088cf04b7d726ea06becbd44fa1ac
victorChekhovoy <viktor.chekhovoi@gmail.com>
2020-09-10 17:40:35 +0000

    attempting to integrate push notifications

--- python-webcam-flask/app.py
+++ python-webcam-flask/app.py
@@ -5,15 +5,25 @@ from flask import Flask, render_template, Response
 from flask_socketio import SocketIO
 from camera import Camera
 from utils import base64_to_pil_image, pil_image_to_base64
-
+from flask_sqlalchemy import SQLAlchemy
+from webpush_handler import trigger_push_notifications_for_subscriptions
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(stdout))
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
+app.config.from_pyfile('application.cfg.py')
 socketio = SocketIO(app)
 camera = Camera(Makeup_artist())
+db = SQLAlchemy(app)
 
+
+class PushSubscription(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    subscription_json = db.Column(db.Text, nullable=False)
+
+db.create_all()
 
 @socketio.on('input image', namespace='/test')
 def test_message(input):
@@ -47,6 +57,47 @@ def gen():
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    
+@app.route("/api/push-subscriptions", methods=["POST"])
+def create_push_subscription():
+    json_data = request.get_json()
+    subscription = PushSubscription.query.filter_by(
+        subscription_json=json_data['subscription_json']
+    ).first()
+    if subscription is None:
+        subscription = PushSubscription(
+            subscription_json=json_data['subscription_json']
+        )
+        db.session.add(subscription)
+        db.session.commit()
+    return jsonify({
+        "status": "success",
+        "result": {
+            "id": subscription.id,
+            "subscription_json": subscription.subscription_json
+        }
+    })
+
+
+@app.route("/admin")
+def admin_page():
+    return render_template("admin.html")
+
+
+@app.route("/admin-api/trigger-push-notifications", methods=["POST"])
+def trigger_push_notifications():
+    json_data = request.get_json()
+    subscriptions = PushSubscription.query.all()
+    results = trigger_push_notifications_for_subscriptions(
+        subscriptions,
+        json_data.get('title'),
+        json_data.get('body')
+    )
+    return jsonify({
+        "status": "success",
+        "result": results
+    })
 
 
 if __name__ == '__main__':
